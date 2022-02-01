c903b28f05ecaaba5009a92d6781ac19b722c635
bambino29 <bambino2983>
2022-01-31 21:48:37 -0500

    Conexion MongoDB

--- app/index.js
+++ app/index.js
@@ -11,3 +11,31 @@ var server = app.listen(3000, function () {
 
   console.log("Example app listening at http://%s:%s", host, port);
 });
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri =
+    "mongodb://mongouser:Q1w2e3r4.@161.35.63.161:2717/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+
+    // Make the appropriate DB calls
+    //await  listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
